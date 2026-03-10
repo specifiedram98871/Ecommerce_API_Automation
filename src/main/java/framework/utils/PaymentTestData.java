412d0a104b2222fd351eee1490f5403bd21c5d70
specifiedram98871 <ramkm98871@gmail.com>
2026-03-10 13:51:13 +0545

    Add PaymentTestData class for managing payment test constants

---
+++ src/main/java/framework/utils/PaymentTestData.java
@@ -0,0 +1,9 @@
+package framework.utils;
+
+public class PaymentTestData {
+    public static final String NAME_ON_CARD = "John Doe";
+    public static final String CARD_NUMBER = "4242 4242 4242 4242";
+    public static final String CVC_CODE = "123";
+    public static final String EXPIRY_MONTH = "12";
+    public static final String EXPIRY_YEAR = "2025";
+}
