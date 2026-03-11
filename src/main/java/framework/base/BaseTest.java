14c8175663811b075babeecfec6b9ffb4e0e6481
specifiedram98871 <ramkm98871@gmail.com>
2026-03-11 08:40:12 +0545

    minor fixes

--- src/main/java/framework/base/BaseTest.java
+++ src/main/java/framework/base/BaseTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+import java.time.Duration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
@@ -28,7 +28,8 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // driver.manage().window().maximize();
     }
     @AfterClass
     public void teardown() {
